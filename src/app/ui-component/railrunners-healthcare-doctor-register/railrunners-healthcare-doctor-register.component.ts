2eb07c394874e33130ce375d422f46e004cb708d
Atul Vijay <atul.vijay@hcl.com>
2022-03-02 19:01:42 +0530

    form changes

--- src/app/ui-component/railrunners-healthcare-doctor-register/railrunners-healthcare-doctor-register.component.ts
+++ src/app/ui-component/railrunners-healthcare-doctor-register/railrunners-healthcare-doctor-register.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RailrunnersHealthcareDoctorRegisterComponent implements OnInit {
 
+  User: any = ['Dr', 'MBBS', 'Orthopedic'];
   constructor() { }
 
   ngOnInit(): void {
