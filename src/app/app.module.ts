9cdb836c715166bc0822076b8bf9df3d36a00bb4
Atul Vijay <atul.vijay@hcl.com>
2022-03-02 17:53:34 +0530

    mat card angular material import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RailrunnersHealthcareLoginComponent } from './ui-component/railrunners-healthcare-login/railrunners-healthcare-login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
+//import {FormsModule} from '@angular/forms';
+
+ 
 
 @NgModule({
   declarations: [
@@ -12,7 +20,11 @@ import { RailrunnersHealthcareLoginComponent } from './ui-component/railrunners-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
