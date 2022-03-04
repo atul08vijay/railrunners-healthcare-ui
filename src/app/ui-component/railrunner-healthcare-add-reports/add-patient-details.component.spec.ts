bed8581bb489ccba5f3ec2bb1e76b99b25f9c9cc
shrutipatil0112 <77260257+shrutipatil0112@users.noreply.github.com>
2022-03-04 16:34:51 +0530

    Add files via upload

---
+++ src/app/ui-component/railrunner-healthcare-add-reports/add-patient-details.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddPatientDetailsComponent } from './add-patient-details.component';
+
+describe('AddPatientDetailsComponent', () => {
+  let component: AddPatientDetailsComponent;
+  let fixture: ComponentFixture<AddPatientDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddPatientDetailsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddPatientDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
